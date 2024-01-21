366e4764eb68682a12246eda71b26846d6f357d4
Augusto008 <aquila.real0@gmail.com>
2024-01-21 10:58:23 -0300

    Create Project

---
+++ src/main/NewWindow.java
@@ -0,0 +1,25 @@
+package main;
+
+import java.awt.Font;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class NewWindow {
+	
+	JFrame frame = new JFrame();
+	JLabel label = new JLabel("Hello!", JLabel.CENTER);
+	
+	NewWindow() {
+		
+		label.setBounds(0,0,100,50);
+		label.setFont(new Font(null,Font.PLAIN,25));
+		
+		frame.add(label);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(520,520);
+		frame.setVisible(true);
+		
+	}
+	
+}
